dcee2acc569de40f2a4718ab3be463c1a734adcf
schlindweinmiguel <m.schlindwein@escola.pr.gov.br>
2025-09-03 08:52:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function atulizarRelogio () {
   const agora =  new Date ();
 
-let horas=  agora.getHours (). toString()p padStart (2, '0');
-let minutos=  agora.getMinutes (). toString()p padStart (2, '0');
-let segundos=  agora.getSeconds (). toString()p padStart (2, '0');
+let horas = agora.getHours ().toString() padStart (2, '0');
+let minutos = agora.getMinutes ().toString() padStart (2, '0');
+let segundos = agora.getSeconds ().toString() padStart (2, '0');
 
 const horaAtual = `${horas}:${minutos}:${segundos} `;
   document.getElementByID('clock').textContet=horaAtual;
